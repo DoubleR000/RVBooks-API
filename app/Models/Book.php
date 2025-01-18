728df62d3fc7368178c69d7767720466fd5e7907
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-19 00:19:24 +0800

    book relationships

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'title_id',
+        'location_id',
+        'status_id',
+        'condition_id',
+        'acquisition_date'
+    ];
+
+    public function title()
+    {
+        return $this->belongsTo(Title::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function condition()
+    {
+        return $this->belongsTo(BookCondition::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(BookStatus::class);
+    }
 }
