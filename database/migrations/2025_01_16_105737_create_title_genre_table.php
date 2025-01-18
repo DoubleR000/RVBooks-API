fde12f2999d6e10ce23f2977b9f31b1b750832c0
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-18 20:41:12 +0800

    renamed migration files and tables

--- database/migrations/2025_01_16_105737_create_title_genre_table.php
+++ database/migrations/2025_01_16_105737_create_title_genre_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('titles_genres', function (Blueprint $table) {
+        Schema::create('title_genre', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("title_id");
             $table->unsignedBigInteger("genre_id");
