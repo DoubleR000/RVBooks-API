57e8f0fafee9c693fdab250b6a1bc7a540f19ee9
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-24 00:52:11 +0800

    removed unnecessary field

--- database/migrations/2025_01_18_151812_create_books_table.php
+++ database/migrations/2025_01_18_151812_create_books_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->foreignIdFor(App\Models\BookStatus::class)->constrained();
             $table->foreignIdFor(App\Models\BookCondition::class)->constrained();
             $table->dateTime('acquisition_date');
-            $table->string('barcode');
 
             $table->timestamps();
         });
