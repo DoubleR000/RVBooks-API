f9e6eecda1d9cfd2e4bb1c876ed47a66dd277f82
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-30 04:53:51 +0800

    included genre as optional parameter to title creation

--- app/Services/TitleService.php
+++ app/Services/TitleService.php
@@ -3,22 +3,29 @@
 namespace App\Services;
 
 use App\Models\Author;
+use App\Models\Genre;
 use App\Models\Title;
 
 class TitleService
 {
-    public function createTitleWithAuthor(array $data)
+    public function createTitle(array $data)
     {
         $title = Title::create($data);
 
-        if (array_key_exists('author_ids', $data) && $authors = Author::find($data['author_ids'])) {
+        if (array_key_exists('author_ids', $data)) {
+            $authors = Author::find($data['author_ids']);
             $title->authors()->sync($authors);
+        }
+
+        if (array_key_exists('genre_ids', $data)) {
+            $genres = Genre::find($data['genre_ids']);
+            $title->genres()->sync($genres);
         }
 
         return $title;
     }
 
-    public function updateTitleWithAuthor(Title $title, array $data)
+    public function updateTitle(Title $title, array $data)
     {
         $title->update($data);
 
@@ -30,7 +37,16 @@ class TitleService
             }
         }
 
+        if (array_key_exists('genre_ids', $data)) {
+            if (empty($data['genre_ids'])) {
+                $title->genres()->detach();
+            } else {
+                $title->genres()->sync($data['genre_ids']);
+            }
+        }
+
         return $title;
     }
+
 }
 
