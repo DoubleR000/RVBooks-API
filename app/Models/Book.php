3550f405faba3467f788c0ed0b9a0bf206760d0a
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-29 23:47:49 +0800

    title - replaced laravel-deleted-models to soft deletes for deletion

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use App\QueryBuilders\BookBuilder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title_id',
         'location_id',
-        'status_id',
-        'condition_id',
+        'book_status_id',
+        'book_condition_id',
         'acquisition_date'
     ];
 
@@ -26,11 +30,16 @@ class Book extends Model
 
     public function condition()
     {
-        return $this->belongsTo(BookCondition::class);
+        return $this->belongsTo(BookCondition::class, 'book_condition_id', 'id');
     }
 
     public function status()
     {
-        return $this->belongsTo(BookStatus::class);
+        return $this->belongsTo(BookStatus::class, 'book_status_id', 'id');
+    }
+
+    public function newEloquentBuilder($query)
+    {
+        return new BookBuilder($query);
     }
 }
