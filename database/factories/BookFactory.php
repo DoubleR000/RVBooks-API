b33aa59b5a7b9c266e2e5b1c8d10c78d1d68ef23
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-31 01:19:22 +0800

    addeed book factory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BookCondition;
+use App\Models\BookStatus;
+use App\Models\Location;
+use App\Models\Title;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title_id' => Title::inRandomOrder()->first()->id,
+            'location_id' => Location::inRandomOrder()->first()->id,
+            'book_status_id' => BookStatus::inRandomOrder()->first()->id,
+            'book_condition_id' => BookCondition::inRandomOrder()->first()->id,
+            'acquisition_date' => fake()->dateTime(),
+        ];
+    }
+}
