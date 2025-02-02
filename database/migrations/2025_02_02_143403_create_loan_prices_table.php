92c4be59a514a79be9567965d34d0562bf3fb3e7
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-02 22:39:54 +0800

    added loan pricing migration

---
+++ database/migrations/2025_02_02_143403_create_loan_prices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loan_prices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Title::class)->constrained();
+            $table->string("rental_period_unit");
+            $table->integer("rental_period_amount");
+            $table->decimal("price", 8, 2);
+            $table->dateTime("effective_from");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loan_prices');
+    }
+};
