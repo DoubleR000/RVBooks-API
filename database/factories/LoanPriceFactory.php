4704df2050941ad2fa5bb5d018a4dd7198666853
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-04 23:15:32 +0800

    removed fake rental_period_unit_id and added rental_duration_in_days

--- database/factories/LoanPriceFactory.php
+++ database/factories/LoanPriceFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\RentalPeriodUnit;
 use App\Models\Title;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -19,8 +18,7 @@ class LoanPriceFactory extends Factory
     public function definition(): array
     {
         return [
-            'rental_period_unit_id' => RentalPeriodUnit::inRandomOrder()->first()->id,
-            'rental_period_amount' => fake()->randomNumber(2),
+            'rental_duration_in_days' => fake()->randomNumber(2),
             'price' => fake()->randomFloat(2, max: 100),
             'effective_from' => fake()->dateTimeThisYear()
         ];
