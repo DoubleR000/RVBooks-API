4b5dad004ac4df8ad857fddb34e57b6bfb2c4ef9
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-06 19:02:32 +0800

    added loan table migration

--- database/migrations/2025_02_04_184224_create_loans_table.php
+++ database/migrations/2025_02_04_184224_create_loans_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,14 @@ return new class extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(App\Models\Book::class)->constrained();
+            $table->foreignIdFor(App\Models\User::class)->constrained();
+            $table->date('return_date');
+            $table->date('due_date');
+            $table->unsignedBigInteger('returned_by_staff');
             $table->timestamps();
+
+            $table->foreign('returned_by_staff')->references('id')->on('users');
         });
     }
 
