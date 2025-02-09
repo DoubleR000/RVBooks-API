786a32f7b867b2d88b51cd6a55b2c6d7cf8917dc
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-09 21:44:35 +0800

    added roles and permissions seeder

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $permissions = [
+            'manage-users',
+
+            'view-authors',
+            'create-authors',
+            'edit-authors',
+            'delete-authors',
+            'restore-authors',
+
+            'view-genres',
+            'create-genres',
+            'edit-genres',
+            'delete-genres',
+            'restore-genres',
+
+            'view-titles',
+            'create-titles',
+            'edit-titles',
+            'delete-titles',
+            'restore-titles',
+
+            'view-book-statuses',
+            'create-book-statuses',
+            'edit-book-statuses',
+            'delete-book-statuses',
+            'restore-book-statuses',
+
+            'view-book-conditions',
+            'create-book-conditions',
+            'edit-book-conditions',
+            'delete-book-conditions',
+            'restore-book-conditions',
+
+            'view-locations',
+            'create-locations',
+            'edit-locations',
+            'delete-locations',
+            'restore-locations',
+
+            'view-books',
+            'create-books',
+            'edit-books',
+            'delete-books',
+            'restore-books',
+
+            'view-loan-prices',
+            'create-loan-prices',
+            'edit-loan-prices',
+            'delete-loan-prices',
+            'restore-loan-prices',
+
+            'view-loans',
+            'view-my-loans',
+            'create-loans',
+            'edit-loans',
+            'delete-loans',
+            'restore-loans'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        $roles = [
+            'admin' => Role::firstOrCreate(['name' => 'admin']),
+            'librarian' => Role::firstOrCreate(['name' => 'librarian']),
+            'patron' => Role::firstOrCreate(['name' => 'patron'])
+        ];
+
+
+        $roles['admin']->syncPermissions($permissions);
+        $roles['librarian']->syncPermissions([
+            'view-authors',
+            'create-authors',
+            'edit-authors',
+            'delete-authors',
+            'restore-authors',
+
+            'view-genres',
+            'create-genres',
+            'edit-genres',
+            'delete-genres',
+            'restore-genres',
+
+            'view-titles',
+            'create-titles',
+            'edit-titles',
+            'delete-titles',
+            'restore-titles',
+
+            'view-book-statuses',
+            'create-book-statuses',
+            'edit-book-statuses',
+            'delete-book-statuses',
+            'restore-book-statuses',
+
+            'view-book-conditions',
+            'create-book-conditions',
+            'edit-book-conditions',
+            'delete-book-conditions',
+            'restore-book-conditions',
+
+            'view-locations',
+            'create-locations',
+            'edit-locations',
+            'delete-locations',
+            'restore-locations',
+
+            'view-books',
+            'create-books',
+            'edit-books',
+            'delete-books',
+            'restore-books',
+
+            'view-loan-prices',
+            'create-loan-prices',
+            'edit-loan-prices',
+            'delete-loan-prices',
+            'restore-loan-prices',
+
+            'view-loans',
+            'view-my-loans',
+            'create-loans',
+            'edit-loans',
+            'delete-loans',
+            'restore-loans'
+        ]);
+
+        $roles['patron']->syncPermissions([
+            'view-authors',
+            'view-genres',
+            'view-titles',
+            'view-books',
+            'view-my-loans',
+            'create-loans'
+        ]);
+    }
+}
