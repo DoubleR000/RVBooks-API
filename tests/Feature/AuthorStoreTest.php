d029df4c43ef70beb7d9f12bf2fe3ae2ba593213
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-11 19:15:58 +0800

    added content checks on happy paths

--- tests/Feature/AuthorStoreTest.php
+++ tests/Feature/AuthorStoreTest.php
@@ -21,23 +21,27 @@ test('patron cannot store new author', function () {
 test('librarian can store new author', function () {
 
     $librarian = User::factory()->librarian()->create();
+    $name = "Librarian Stored Author";
 
     actingAs($librarian)
-        ->postJson(route('authors.store'), ['name' => 'Librarian Stored Author'])
+        ->postJson(route('authors.store'), ['name' => $name])
         ->assertStatus(201)
         ->assertJson(fn(AssertableJson $json) =>
             $json->has('data', fn(AssertableJson $json) =>
-                $json->hasAll(['id', 'name'])));
+                $json->hasAll(['id', 'name'])
+                    ->where('name', $name)));
 });
 
 test('admin can store new author', function () {
 
     $admin = User::factory()->admin()->create();
+    $name = "Admin Stored Author";
 
     actingAs($admin)
-        ->postJson(route('authors.store'), ['name' => 'Admmin Stored Author'])
+        ->postJson(route('authors.store'), ['name' => $name])
         ->assertStatus(201)
         ->assertJson(fn(AssertableJson $json) =>
             $json->has('data', fn(AssertableJson $json) =>
-                $json->hasAll(['id', 'name'])));
+                $json->hasAll(['id', 'name'])
+                    ->where('name', $name)));
 });
