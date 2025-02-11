65c183644186f81abaa742e42b1e0b63dc1960f6
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-11 18:15:20 +0800

    updated author view index tests

---
+++ tests/Feature/AuthorIndexTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
+use function Pest\Laravel\actingAs;
+
+test('unauthenticated user cannot view author list', function () {
+    $this->get(route('authors.index'))->assertStatus(401);
+});
+
+
+test('admin can view author list', function () {
+
+    $admin = User::factory()->admin()->create();
+
+    actingAs($admin)
+        ->getJson(route('authors.index'))
+        ->assertStatus(200)
+        ->assertJson(
+            fn(AssertableJson $json) =>
+            $json->hasAll(['data', 'links', 'meta'])
+                ->has('data.0', fn(AssertableJson $json) =>
+                    $json->hasAll(['id', 'name']))
+        );
+});
+
+test('librarian can view author list', function () {
+
+    $librarian = User::factory()->librarian()->create();
+
+    actingAs($librarian)
+        ->getJson(route('authors.index'))
+        ->assertStatus(200)
+        ->assertJson(
+            fn(AssertableJson $json) =>
+            $json->hasAll(['data', 'links', 'meta'])
+                ->has('data.0', fn(AssertableJson $json) =>
+                    $json->hasAll(['id', 'name']))
+        );
+});
+
+test('patron can view author list', function () {
+
+    $patron = User::factory()->patron()->create();
+
+    actingAs($patron)
+        ->getJson(route('authors.index'))
+        ->assertStatus(200)
+        ->assertJson(
+            fn(AssertableJson $json) =>
+            $json->hasAll(['data', 'links', 'meta'])
+                ->has('data.0', fn(AssertableJson $json) =>
+                    $json->hasAll(['id', 'name']))
+        );
+});
