9a05b6114baa94a2370167ed51e4d07ebb0bc71a
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-11 22:26:11 +0800

    included /user route to 'auth:sanctum' middleware group and added name

--- routes/api.php
+++ routes/api.php
@@ -12,41 +12,42 @@ use App\Http\Controllers\TitleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
 
 Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
 
-Route::middleware(['auth:sanctum'])->group(
-    function () {
-        Route::apiResource('titles', TitleController::class);
-        Route::post('titles/{title}/restore', [TitleController::class, 'restore'])->withTrashed()->name('titles.restore');
-        Route::patch('titles/{title}/update-slug', [TitleController::class, 'updateSlug'])->name('titles.update-slug');
+Route::middleware(['auth:sanctum'])->group(function () {
 
-        Route::apiResource('authors', AuthorController::class);
-        Route::post('authors/restore/{author}', [AuthorController::class, 'restore'])->name('authors.restore');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->name('user');
 
-        Route::apiResource('genres', GenreController::class);
-        Route::post('genres/restore/{genre}', [GenreController::class, 'restore'])->name('genres.restore');
+    Route::apiResource('titles', TitleController::class);
+    Route::post('titles/{title}/restore', [TitleController::class, 'restore'])->withTrashed()->name('titles.restore');
+    Route::patch('titles/{title}/update-slug', [TitleController::class, 'updateSlug'])->name('titles.update-slug');
 
-        Route::apiResource('locations', LocationController::class);
-        Route::post('locations/restore/{location}', [LocationController::class, 'restore'])->name('locations.restore');
+    Route::apiResource('authors', AuthorController::class);
+    Route::post('authors/restore/{author}', [AuthorController::class, 'restore'])->name('authors.restore');
 
-        Route::apiResource('book-conditions', BookConditionController::class);
-        Route::post('book-conditions/restore/{book_condition}', [BookConditionController::class, 'restore'])->name('book-conditions.restore');
+    Route::apiResource('genres', GenreController::class);
+    Route::post('genres/restore/{genre}', [GenreController::class, 'restore'])->name('genres.restore');
 
-        Route::apiResource('book-statuses', BookStatusController::class);
-        Route::post('book-statuses/restore/{book_status}', [BookStatusController::class, 'restore'])->name('book-statuses.restore');
+    Route::apiResource('locations', LocationController::class);
+    Route::post('locations/restore/{location}', [LocationController::class, 'restore'])->name('locations.restore');
 
-        Route::apiResource('books', BookController::class);
-        Route::post('books/{book}/restore', [BookController::class, 'restore'])->withTrashed()->name('books.restore');
+    Route::apiResource('book-conditions', BookConditionController::class);
+    Route::post('book-conditions/restore/{book_condition}', [BookConditionController::class, 'restore'])->name('book-conditions.restore');
 
-        Route::apiResource('loan-prices', LoanPriceController::class);
-        Route::post('loan-prices/{loan_price}/restore', [LoanPriceController::class, 'restore'])->withTrashed()->name('loan-prices.restore');
-    }
-);
+    Route::apiResource('book-statuses', BookStatusController::class);
+    Route::post('book-statuses/restore/{book_status}', [BookStatusController::class, 'restore'])->name('book-statuses.restore');
+
+    Route::apiResource('books', BookController::class);
+    Route::post('books/{book}/restore', [BookController::class, 'restore'])->withTrashed()->name('books.restore');
+
+    Route::apiResource('loan-prices', LoanPriceController::class);
+    Route::post('loan-prices/{loan_price}/restore', [LoanPriceController::class, 'restore'])->withTrashed()->name('loan-prices.restore');
+});
 
 
