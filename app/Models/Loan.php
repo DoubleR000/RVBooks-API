a8f0fd0b1c6ab37769f38403a258ca047ebda735
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-13 21:10:12 +0800

    added Loan fillable fields and user relationship

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Loan extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'return_date',
+        'due_date',
+        'returned_by_staff'
+    ];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
 }
