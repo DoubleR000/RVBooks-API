b535835102dc5e64a2377d42465621d4efe3f8e4
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-02-18 19:32:56 +0800

    added store function

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,24 +3,48 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Str;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $itemsPerPage = $request->page_size ?? 20;
+        $authors = User::paginate($itemsPerPage);
+
+        return UserResource::collection($authors);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        $validated = $request->validated();
+
+        $password = Str::password(10);
+
+        $user = User::create([
+            'name' => $validated->name,
+            'email' => $validated->email,
+            'password' => Hash::make($password)
+        ]);
+
+        $response = [
+            'user' => UserResource::make($user),
+            'password' => $password
+        ];
+
+        return response()->json($response);
     }
 
     /**
